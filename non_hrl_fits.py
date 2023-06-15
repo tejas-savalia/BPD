27691f4b2daf4390317bd48fd74c9ff140b08501
tejassavalia7 <tejas.savalia7@gmail.com>
2023-06-15 03:52:54 -0400

    Linear models done for individual subject fits

--- non_hrl_fits.py
+++ non_hrl_fits.py
@@ -7,20 +7,30 @@ import multiprocessing as mp
 # df_to_fit  = pd.read_csv('df_neutral.csv')
 df_neutral  = pd.read_csv('df_neutral.csv')
 df_stressed  = pd.read_csv('df_stressed.csv')
-# df_to_fit = pd.concat([df_neutral, df_stressed])
+df_to_fit = pd.concat([df_neutral, df_stressed])
 
-# def fit_nonhrl(subject):
-# # def fit_nonhrl(chain):
+def fit_nonhrl(subject):
+# def fit_nonhrl(chain):
     
-#     print('Subject: ', subject)
-#     temp_df = df_to_fit.loc[df_to_fit.subj_idx == subject].reset_index().drop('index', axis = 1)
-#     m_non_hrl = hddm.Hrl(temp_df, depends_on = {'v':['Session'], 'alpha':['Session'], 'pos_alpha':['Session']}, p_outlier = 0.05, dual=True)
-#     m_non_hrl.find_starting_values()
-#     m_non_hrl.sample(10000, burn=4000, thin = 6, dbname="traces.db", db="pickle")
-#     subj_traces = m_non_hrl.get_traces()
-#     subj_traces['subject'] = subject
-#     # subj_traces['chain'] = chain
-#     return subj_traces
+    print('Subject: ', subject)
+    temp_df = df_to_fit.loc[df_to_fit.subj_idx == subject].reset_index().drop('index', axis = 1)
+    # temp_firsthalf_df = temp_df[:int(len(temp_df)/2)]
+    temp_df_AB = temp_df.loc[temp_df['split_by'] == 0].reset_index().drop('index', axis = 1)
+    m_non_hrl = hddm.Hrl(temp_df_AB, depends_on = {'v':['Session'], 'alpha':['Session'], 'pos_alpha':['Session']}, p_outlier = 0.05, dual=True)
+    m_non_hrl.find_starting_values()
+    m_non_hrl.sample(10000, burn=4000, thin = 6, dbname="traces.db", db="pickle")
+    subj_traces = m_non_hrl.get_traces()
+    subj_traces['subject'] = subject
+    # subj_traces['chain'] = chain
+    return subj_traces
+
+subj_id = np.arange(148)
+# chain_id = np.arange(8)
+pool = mp.Pool()
+m_hrl_results_neutral = pd.concat(pool.map(fit_nonhrl, subj_id))
+# m_hrl_results = pd.concat(pool.map(fit_nonhrl, chain_id))
+m_hrl_results_neutral.to_csv('model_traces/10k4k5p6thin_indsubj_AB_traces.csv')
+
 
 def fit_nonhrl_neutral(subject):
 # def fit_nonhrl(chain):
@@ -35,12 +45,12 @@ def fit_nonhrl_neutral(subject):
     # subj_traces['chain'] = chain
     return subj_traces
 
-subj_id = np.arange(148)
-# chain_id = np.arange(8)
-pool = mp.Pool()
-m_hrl_results_neutral = pd.concat(pool.map(fit_nonhrl_neutral, subj_id))
-# m_hrl_results = pd.concat(pool.map(fit_nonhrl, chain_id))
-m_hrl_results_neutral.to_csv('model_traces/10k4k5p6thin_indsubj_neutral_traces.csv')
+# subj_id = np.arange(148)
+# # chain_id = np.arange(8)
+# pool = mp.Pool()
+# m_hrl_results_neutral = pd.concat(pool.map(fit_nonhrl_neutral, subj_id))
+# # m_hrl_results = pd.concat(pool.map(fit_nonhrl, chain_id))
+# m_hrl_results_neutral.to_csv('model_traces/10k4k5p6thin_indsubj_neutral_traces.csv')
 
 
 def fit_nonhrl_stressed(subject):
@@ -56,9 +66,9 @@ def fit_nonhrl_stressed(subject):
     # subj_traces['chain'] = chain
     return subj_traces
 
-subj_id = np.arange(148)
-# chain_id = np.arange(8)
-pool = mp.Pool()
-m_hrl_results_stressed = pd.concat(pool.map(fit_nonhrl_stressed, subj_id))
-# m_hrl_results = pd.concat(pool.map(fit_nonhrl, chain_id))
-m_hrl_results_stressed.to_csv('model_traces/10k4k5p6thin_indsubj_stressed_traces.csv')
+# subj_id = np.arange(148)
+# # chain_id = np.arange(8)
+# pool = mp.Pool()
+# m_hrl_results_stressed = pd.concat(pool.map(fit_nonhrl_stressed, subj_id))
+# # m_hrl_results = pd.concat(pool.map(fit_nonhrl, chain_id))
+# m_hrl_results_stressed.to_csv('model_traces/10k4k5p6thin_indsubj_stressed_traces.csv')
--- parallel_linear_model.py
+++ parallel_linear_model.py
@@ -25,99 +25,177 @@ import multiprocessing as mp
 # In[2]:
 
 
-data = pd.read_csv('new_d.csv').drop('Unnamed: 0', axis = 1)
+# data = pd.read_csv('new_d.csv').drop('Unnamed: 0', axis = 1)
 
 
 
-data = data.sort_values(['ID', 'Session', 'Block'], ascending=[True, True, True]).reset_index().drop('index', axis = 1)
-# data['optimal response taken'] = data['StimulusPresentation1.RESP'] == data['StimulusPresentation1.CRESP']
+# data = data.sort_values(['ID', 'Session', 'Block'], ascending=[True, True, True]).reset_index().drop('index', axis = 1)
+# # data['optimal response taken'] = data['StimulusPresentation1.RESP'] == data['StimulusPresentation1.CRESP']
 
 
 
 
 
-# # Models using Traces
+# # # Models using Traces
 
 
-traces = pd.read_csv('non_hierarchical_traces_thin5.csv').drop('Unnamed: 0', axis = 1).reset_index()
+# traces = pd.read_csv('non_hierarchical_traces_thin5.csv').drop('Unnamed: 0', axis = 1).reset_index()
 
-rng = np.random.default_rng()
-new_traces = pd.DataFrame({'trace#': np.arange(3000)})
-#To not shuffle
-# for column in traces.columns:
-#     new_traces[column] = rng.permuted(traces[column].values)
-# traces_melt = new_traces.melt(id_vars='trace#', var_name='parameters', value_name='parameter values')
-traces_melt = traces.melt(id_vars='trace#', var_name='parameters', value_name='parameter values')
+# rng = np.random.default_rng()
+# new_traces = pd.DataFrame({'trace#': np.arange(3000)})
+# #To not shuffle
+# # for column in traces.columns:
+# #     new_traces[column] = rng.permuted(traces[column].values)
+# # traces_melt = new_traces.melt(id_vars='trace#', var_name='parameters', value_name='parameter values')
+# traces_melt = traces.melt(id_vars='trace#', var_name='parameters', value_name='parameter values')
 
-traces_melt = traces_melt[traces_melt.parameters.str.contains('subj')]
-traces_melt.loc[traces_melt.parameters.str.contains('Neutral'), 'Session'] = 'Neutral'
-traces_melt.loc[traces_melt.parameters.str.contains('Stressed'), 'Session'] = 'Stressed'
-traces_melt[['parameters', 'ID']] = traces_melt.parameters.str.split('.', expand=True)
-traces_melt.loc[traces_melt.parameters.str.contains('alpha'), 'parameter values'] = np.exp(traces_melt.loc[traces_melt.parameters.str.contains('alpha'), 'parameter values'])/(1+np.exp(traces_melt.loc[traces_melt.parameters.str.contains('alpha'), 'parameter values']))
-
-
-# In[10]:
+# traces_melt = traces_melt[traces_melt.parameters.str.contains('subj')]
+# traces_melt.loc[traces_melt.parameters.str.contains('Neutral'), 'Session'] = 'Neutral'
+# traces_melt.loc[traces_melt.parameters.str.contains('Stressed'), 'Session'] = 'Stressed'
+# traces_melt[['parameters', 'ID']] = traces_melt.parameters.str.split('.', expand=True)
+# traces_melt.loc[traces_melt.parameters.str.contains('alpha'), 'parameter values'] = np.exp(traces_melt.loc[traces_melt.parameters.str.contains('alpha'), 'parameter values'])/(1+np.exp(traces_melt.loc[traces_melt.parameters.str.contains('alpha'), 'parameter values']))
 
 
-for ids in traces_melt.ID.unique():
-    traces_melt.loc[traces_melt['ID'] == ids, 'BPD'] = data.loc[data['ID'] == int(ids), 'BPD#'].unique()[0]
-    traces_melt.loc[traces_melt['ID'] == ids, 'Condition'] = data.loc[data['ID'] == int(ids), 'Condition'].unique()[0]
+# # In[10]:
+
+
+# for ids in traces_melt.ID.unique():
+#     traces_melt.loc[traces_melt['ID'] == ids, 'BPD'] = data.loc[data['ID'] == int(ids), 'BPD#'].unique()[0]
+#     traces_melt.loc[traces_melt['ID'] == ids, 'Condition'] = data.loc[data['ID'] == int(ids), 'Condition'].unique()[0]
     
+# traces_melt['mean_centered_BPD'] = traces_melt['BPD'] - np.mean(traces_melt['BPD'].values)
+
+# def lm_params(trace_no):
+#     parameters = 'pos_alpha_subj'
+#     temp_data = traces_melt.loc[((traces_melt['trace#'] ==trace_no) & (traces_melt['parameters'].str.startswith(parameters)))].rename(columns={'parameter values':'param_vals'})
+    
+#     res = smf.ols(formula='param_vals ~  mean_centered_BPD*Session*C(Condition)', data=temp_data).fit()
+#     if trace_no%100 == 0:
+#         print(trace_no)
+#     return res.params
+
+data = pd.read_csv('new_d.csv')
+data = data[['ID', 'Condition', 'Session', 'BPD#']]
+new_data = []
+subject = 0
+for ids in data.ID.unique():
+    if len(data.loc[data['ID'] == ids, 'Session'].unique()) < 2:
+        continue
+    else:
+        temp_data = data.loc[data['ID'] == ids]
+        temp_data['subject'] = subject
+        new_data.append(temp_data)
+        subject = subject + 1
+new_data = pd.concat(new_data).drop('ID', axis = 1)
+
+traces = pd.read_csv('model_traces/10k40k5p6thin_indsubj_8chain_traces.csv').drop('Unnamed: 0', axis = 1)
+traces['trace#'] = np.tile(np.arange(1000), 148)
+# traces['alpha(Neutral)'] = np.exp(traces['alpha(Neutral)'])/(1+np.exp(traces['alpha(Neutral)']))
+# traces['pos_alpha(Neutral)'] = np.exp(traces['pos_alpha(Neutral)'])/(1+np.exp(traces['pos_alpha(Neutral)']))
+# traces['alpha(Stressed)'] = np.exp(traces['alpha(Stressed)'])/(1+np.exp(traces['alpha(Stressed)']))
+# traces['pos_alpha(Stressed)'] = np.exp(traces['pos_alpha(Stressed)'])/(1+np.exp(traces['pos_alpha(Stressed)']))
+
+
+traces_melt = traces.melt(id_vars=['subject', 'trace#'], var_name='parameters', value_name='param_vals')
+traces_melt['Session'] = 'Stressed'
+traces_melt.loc[traces_melt['parameters'].str.contains('Neutral'), 'Session'] = 'Neutral'
+for sub in new_data['subject'].unique():
+    traces_melt.loc[traces_melt['subject'] == sub, 'BPD'] = new_data.loc[new_data['subject'] == sub, 'BPD#'].unique()[0]
+    traces_melt.loc[traces_melt['subject'] == sub, 'Condition'] = new_data.loc[new_data['subject'] == sub, 'Condition'].unique()[0]
+
+traces_melt.loc[traces_melt['Condition'] == 1, 'Condition'] = 'Non_Social'
+traces_melt.loc[traces_melt['Condition'] == 2, 'Condition'] = 'Social'
 traces_melt['mean_centered_BPD'] = traces_melt['BPD'] - np.mean(traces_melt['BPD'].values)
 
-def lm_params(trace_no):
-    parameters = 'pos_alpha_subj'
-    temp_data = traces_melt.loc[((traces_melt['trace#'] ==trace_no) & (traces_melt['parameters'].str.startswith(parameters)))].rename(columns={'parameter values':'param_vals'})
-    
-    res = smf.ols(formula='param_vals ~  mean_centered_BPD*Session*C(Condition)', data=temp_data).fit()
+
+# def fit_lm_beta(trace_no):
+#     param = 'v'
+#     temp_df = traces_melt.loc[((traces_melt['trace#'] == trace_no) & (traces_melt['parameters'].str.startswith(param)))]
+#     res = smf.ols(formula='param_vals ~  mean_centered_BPD*Session*Condition', data=temp_df).fit()
+#     if trace_no%100 == 0:
+#         print(trace_no)
+#     return res.params
+
+
+
+# trace_nos = np.arange(1000)
+# pool = mp.Pool()
+# beta_results = pd.concat(pool.map(fit_lm_beta, trace_nos), axis = 1)
+# print(beta_results)
+# beta = beta_results.reset_index().rename(columns={'index':'coefficient'})
+# beta.to_csv('LR coefficients/mean_centered_indsub_beta.csv')
+
+def fit_lm_alpha(trace_no):
+    param = 'alpha'
+    temp_df = traces_melt.loc[((traces_melt['trace#'] == trace_no) & (traces_melt['parameters'].str.startswith(param)))]
+    res = smf.ols(formula='param_vals ~  mean_centered_BPD*Session*Condition', data=temp_df).fit()
+    if trace_no%100 == 0:
+        print(trace_no)
+    return res.params
+
+
+trace_nos = np.arange(1000)
+pool = mp.Pool()
+alpha_loss_results = pd.concat(pool.map(fit_lm_alpha, trace_nos), axis = 1)
+print(alpha_loss_results)
+alpha_loss = alpha_loss_results.reset_index().rename(columns={'index':'coefficient'})
+alpha_loss.to_csv('LR coefficients/mean_centered_indsub_alpha_loss.csv')
+
+print('Alpha Loss done')
+
+
+def fit_lm_pos_alpha(trace_no):
+    param = 'pos_alpha'
+    temp_df = traces_melt.loc[((traces_melt['trace#'] == trace_no) & (traces_melt['parameters'].str.startswith(param)))]
+    res = smf.ols(formula='param_vals ~  mean_centered_BPD*Session*Condition', data=temp_df).fit()
     if trace_no%100 == 0:
         print(trace_no)
     return res.params
 
 
 
-trace_nos = np.arange(3000)
+trace_nos = np.arange(1000)
 pool = mp.Pool()
-pos_alpha_results = pd.concat(pool.map(lm_params, trace_nos), axis = 1)
-print(pos_alpha_results)
-pos_alpha = pos_alpha_results.reset_index().rename(columns={'index':'coefficient'})
-pos_alpha.to_csv('LR coefficients/mean_centered_shuffled_alpha_g.csv')
+alpha_gain_results = pd.concat(pool.map(fit_lm_pos_alpha, trace_nos), axis = 1)
+print(alpha_gain_results)
+alpha_gain = alpha_gain_results.reset_index().rename(columns={'index':'coefficient'})
+alpha_gain.to_csv('LR coefficients/mean_centered_indsub_alpha_gain.csv')
 
 
-def lm_params(trace_no):
-    parameters = 'alpha_subj'
-    temp_data = traces_melt.loc[((traces_melt['trace#'] ==trace_no) & (traces_melt['parameters'].str.startswith(parameters)))].rename(columns={'parameter values':'param_vals'})
+# def lm_params(trace_no):
+#     parameters = 'alpha_subj'
+#     temp_data = traces_melt.loc[((traces_melt['trace#'] ==trace_no) & (traces_melt['parameters'].str.startswith(parameters)))].rename(columns={'parameter values':'param_vals'})
     
-    res = smf.ols(formula='param_vals ~  mean_centered_BPD*Session*C(Condition)', data=temp_data).fit()
-    if trace_no%100 == 0:
-        print(trace_no)
-    return res.params
+#     res = smf.ols(formula='param_vals ~  mean_centered_BPD*Session*C(Condition)', data=temp_data).fit()
+#     if trace_no%100 == 0:
+#         print(trace_no)
+#     return res.params
 
 
 
-trace_nos = np.arange(3000)
-pool = mp.Pool()
-alpha_results = pd.concat(pool.map(lm_params, trace_nos), axis = 1)
-print(alpha_results)
-alpha = alpha_results.reset_index().rename(columns={'index':'coefficient'})
-alpha.to_csv('LR coefficients/mean_centered_shuffled_alpha_l.csv')
+# trace_nos = np.arange(3000)
+# pool = mp.Pool()
+# alpha_results = pd.concat(pool.map(lm_params, trace_nos), axis = 1)
+# print(alpha_results)
+# alpha = alpha_results.reset_index().rename(columns={'index':'coefficient'})
+# alpha.to_csv('LR coefficients/mean_centered_shuffled_alpha_l.csv')
 
 
-def lm_params(trace_no):
-    parameters = 'v'
-    temp_data = traces_melt.loc[((traces_melt['trace#'] ==trace_no) & (traces_melt['parameters'].str.startswith(parameters)))].rename(columns={'parameter values':'param_vals'})
+# def lm_params(trace_no):
+#     parameters = 'v'
+#     temp_data = traces_melt.loc[((traces_melt['trace#'] ==trace_no) & (traces_melt['parameters'].str.startswith(parameters)))].rename(columns={'parameter values':'param_vals'})
     
-    res = smf.ols(formula='param_vals ~  mean_centered_BPD*Session*C(Condition)', data=temp_data).fit()
-    if trace_no%100 == 0:
-        print(trace_no)
-    return res.params
+#     res = smf.ols(formula='param_vals ~  mean_centered_BPD*Session*C(Condition)', data=temp_data).fit()
+#     if trace_no%100 == 0:
+#         print(trace_no)
+#     return res.params
 
 
 
-trace_nos = np.arange(3000)
-pool = mp.Pool()
-beta_results = pd.concat(pool.map(lm_params, trace_nos), axis = 1)
-print(beta_results)
-beta = beta_results.reset_index().rename(columns={'index':'coefficient'})
-beta.to_csv('LR coefficients/mean_centered_shuffled_beta.csv')
+# trace_nos = np.arange(3000)
+# pool = mp.Pool()
+# beta_results = pd.concat(pool.map(lm_params, trace_nos), axis = 1)
+# print(beta_results)
+# beta = beta_results.reset_index().rename(columns={'index':'coefficient'})
+# beta.to_csv('LR coefficients/mean_centered_shuffled_beta.csv')
 
